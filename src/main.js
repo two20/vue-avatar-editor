618d1c1c5e2202fdf3a6f714838817e11f3ec4bf
Dawood Sangameshwari <dawood@220tech.co.in>
2017-01-12 16:18:47 +0530

    initial commit

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueAvatar from './components/VueAvatar.vue'
+import VueAvatarScale from './components/VueAvatarScale.vue'
+
+let vm = new Vue({
+  el: '#app',
+  components: {
+  	VueAvatar,
+  	VueAvatarScale
+  },
+  methods:{
+  	onChangeScale (scale) {
+      	this.$refs.vueavatar.changeScale(scale)
+    },
+    saveClicked(){
+    	console.log('save clicked')
+    	var img = this.$refs.vueavatar.getImageScaled()
+    	document.getElementById('img-1').src = img.toDataURL()
+    },
+    onImageReady(scale){
+    	this.$refs.vueavatarscale.setScale(scale)
+    }
+  }
+})
