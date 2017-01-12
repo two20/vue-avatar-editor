49bf72b2f2e7f656e6d01d28a0b1afbdd376ed7a
Dawood Sangameshwari <dawood@220tech.co.in>
2017-01-12 18:03:02 +0530

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-
-	/***************** Waypoints ******************/
-
 	$('.wp1').waypoint(function() {
 		$('.wp1').addClass('animated fadeInLeft');
 	}, {
@@ -22,65 +19,9 @@ $(document).ready(function() {
 	}, {
 		offset: '75%'
 	});
-
-	/***************** Flickity ******************/
-
-	$('#featuresSlider').flickity({
-		cellAlign: 'left',
-		contain: true,
-		prevNextButtons: false
-	});
-
-	$('#showcaseSlider').flickity({
-		cellAlign: 'left',
-		contain: true,
-		prevNextButtons: false,
-		imagesLoaded: true
-	});
-
-	/***************** Fancybox ******************/
-
-	$(".youtube-media").on("click", function(e) {
-		var jWindow = $(window).width();
-		if (jWindow <= 768) {
-			return;
-		}
-		$.fancybox({
-			href: this.href,
-			padding: 4,
-			type: "iframe",
-			'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
-		});
-		return false;
-	});
-
 });
 
-$(document).ready(function() {
-	$("a.single_image").fancybox({
-		padding: 4,
-	});
-});
 
-/***************** Nav Transformicon ******************/
-
-/* When user clicks the Icon */
-$(".nav-toggle").click(function() {
-	$(this).toggleClass("active");
-	$(".overlay-boxify").toggleClass("open");
-});
-
-/* When user clicks a link */
-$(".overlay ul li a").click(function() {
-	$(".nav-toggle").toggleClass("active");
-	$(".overlay-boxify").toggleClass("open");
-});
-
-/* When user clicks outside */
-$(".overlay").click(function() {
-	$(".nav-toggle").toggleClass("active");
-	$(".overlay-boxify").toggleClass("open");
-});
 
 /***************** Smooth Scrolling ******************/
 
