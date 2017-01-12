c73ba4daea007821c9ce4a3d6ad5e5c67980e75c
Dawood Sangameshwari <dawood@220tech.co.in>
2017-01-12 17:14:02 +0530

    used theme

---
+++ js/scrolling-nav.js
@@ -0,0 +1,19 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse").addClass('navbar-default');
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse").removeClass('navbar-default');
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 30
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
